14409bd4e1ad91f1b7ebf2903ad4827735cdbf2c
Yadong Cao <caoyadongemail@163.com>
2020-05-28 19:12:22 +0800

    添加遗漏的

---
+++ ProcessTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 100000m; i++)
+            {
+                decimal j = 0m;
+                j = i* i;
+            }
+            Thread.Sleep(10000000);
+            Console.WriteLine(args[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
