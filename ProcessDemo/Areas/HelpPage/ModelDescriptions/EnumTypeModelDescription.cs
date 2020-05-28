9c4ad1db530c3c7fab8112538c10a740546c5a68
Yadong Cao <caoyadongemail@163.com>
2020-05-28 18:56:49 +0800

    上传进程代码

---
+++ ProcessDemo/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ProcessDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumTypeModelDescription : ModelDescription
+    {
+        public EnumTypeModelDescription()
+        {
+            Values = new Collection<EnumValueDescription>();
+        }
+
+        public Collection<EnumValueDescription> Values { get; private set; }
+    }
+}
---
+++ ProcessDemo/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProcessDemo.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public Dictionary<string, string> Get()
+        {
+
+
+            var testDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine(testDir);
+            var process = new Process();
+            process.StartInfo.FileName = Path.Combine(testDir, "bin/ProcessTest.exe");
+            process.Start();
+
+
+            int nProcessID = process.Id;
+            Dictionary<string, string> resultDict = new Dictionary<string, string>();
+            resultDict.Add("ProcessID", nProcessID.ToString());
+            return resultDict;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public Dictionary<string, string> Post([FromBody]Dictionary<string, string> dictValue)
+        {
+            var testDir = System.AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine(testDir);
+            var process = new Process();
+            process.StartInfo.FileName = Path.Combine(testDir, "bin/ProcessTest.exe");
+            process.StartInfo.Arguments = dictValue["Test"];
+            process.Start();
+
+
+            int nProcessID = process.Id;
+            Dictionary<string, string> resultDict = new Dictionary<string, string>();
+            resultDict.Add("ProcessID", nProcessID.ToString());
+            return resultDict;
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
